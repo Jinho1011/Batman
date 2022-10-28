af5c85214481de80759d07c083c5d16fc1b08dd7
jinho1011 <gino9940@gmail.com>
2022-10-28 17:30:22 +0900

    refactor: convert to ts

---
+++ WEB/batman/src/pages/home.tsx
@@ -0,0 +1,42 @@
+import { useSelector } from "react-redux";
+import { withRouter } from "react-router-dom";
+
+import PageTemplate from "../container/common/PageTemplate.cntr";
+import Cam from "../container/home/Cam";
+import Runway from "../container/home/Runway";
+import LogList from "../container/home/Log";
+import User from "../component/home/User";
+import SideWrapper from "../component/home/SideWrapper";
+
+const HomePage = ({ history }: { history: any }) => {
+  const { isFull } = useSelector((state) => ({
+    isFull: (state as any).fullscreen.isFull,
+  }));
+
+  return (
+    <PageTemplate>
+      {isFull && (
+        <>
+          <Cam />
+        </>
+      )}
+      {!isFull && (
+        <>
+          <Cam />
+          <Runway />
+          <SideWrapper>
+            <LogList />
+            <User />
+          </SideWrapper>
+        </>
+      )}
+    </PageTemplate>
+  );
+};
+
+// TODO
+// 전체화면 동작하게 하기
+
+// Have to Set Fullsize module
+// 여기 컴포넌트들을 모두 컨테이너로 바꾸기
+export default withRouter(HomePage);
