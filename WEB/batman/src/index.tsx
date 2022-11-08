cf397bddf3cd7b353ea50bd8d85ea99f6ba4362a
jinho1011 <gino9940@gmail.com>
2022-11-08 19:58:32 +0900

    feat: add queryProvider

--- WEB/batman/src/index.tsx
+++ WEB/batman/src/index.tsx
@@ -9,10 +9,11 @@ import "../node_modules/react-resizable/css/styles.css";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import rootReducer, { rootSaga } from "./module";
-import { composeWithDevTools } from "redux-devtools-extension"; // 리덕스 개발자 도구
+import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga";
 import theme from "./public/style/palette";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -23,14 +24,18 @@ const store = createStore(
 
 sagaMiddleware.run(rootSaga);
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
